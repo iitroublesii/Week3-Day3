f4ae9a66c21f56ca339209c2c095b82af23e7fed
Jo Greenlee <sweettroubles29@gmail.com>
2026-02-20 20:53:08 -0600

    Added a button and js

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 "use strict";
 
-console.log("Hey")
+const output = document.getElementById("output");
+const output2 = document.getElementById("output2");
+const output3 = document.getElementById("output3");
+
+const jsonForm = document.getElementById("jsonForm");
+
+jsonForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  try {
+    const items = JSON.parse(jsonForm.jsonInput.value);
+    console.log(items);
+    output.textContent = items[0];
+    output2.textContent = items[1];
+    output3.textContent = items[2];
+  } catch (error) {
+    output.textContent = "";
+    output2.textContent = "";
+    output3.textContent = "";
+  }
+});
